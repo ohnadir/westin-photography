e56f3163157afc3c0b739b9bd8c5ff9a805e7c5d
ohnadir <qx.nadir@gmail.com>
2022-04-16 23:21:58 +0600

    add silder on home section

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Cart = () => {
     return (
         <div>
-            <h1>This is Crat section</h1>
+            <div>
+                
+            </div>
         </div>
     );
 };
