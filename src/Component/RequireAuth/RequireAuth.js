429dbb6f40e8aea12067e9f86882cfc26f760143
ohnadir <qx.nadir@gmail.com>
2022-04-17 12:26:14 +0600

    fix reload redirect to login page

--- src/Component/RequireAuth/RequireAuth.js
+++ src/Component/RequireAuth/RequireAuth.js
@@ -3,9 +3,11 @@ import { Navigate, useLocation } from "react-router-dom";
 import auth from "../Firebase/firebase.init";
 
 function RequireAuth({ children }) {
-    const [user] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
     let location = useLocation();
-  
+    if(loading) {
+    return;
+    }
     if (!user) {
       return <Navigate to="/login" state={{ from: location }} replace />;
     }
